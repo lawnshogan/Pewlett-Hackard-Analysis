ff02b7ec5eda99055e6518d04c59e78446233ec3
lawnshogan <Shawnlogan.gis@gmail.com>
2022-05-02 17:23:42 -0600

    Starting 7.3.4

--- schema.sql
+++ schema.sql
@@ -1,3 +1,12 @@
+-- DROP TABLES
+DROP TABLE departments
+DROP TABLE employees
+DROP TABLE dept_manager
+DROP TABLE salaries
+DROP TABLE dept_employee
+DROP TABLE titles
+
+
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
 	dept_no VARCHAR(4)NOT NULL,
@@ -44,6 +53,11 @@ CREATE TABLE titles (
 
 SELECT * FROM departments;
 
+
+
+
+
+
 -- Select employees whos birthdays are Between X and X
 SELECT first_name, last_name
 FROM employees
@@ -65,6 +79,11 @@ SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
 
+
+
+
+
+
 -- Retirement eligibility
 SELECT first_name, last_name
 FROM employees
@@ -84,8 +103,13 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
-
 DROP TABLE retirement_info;
+
+
+
+
+
+
 
 -- Create new table for retiring employees
 SELECT emp_no, first_name, last_name
@@ -95,4 +119,79 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 -- Check the table
 SELECT * FROM retirement_info;
+
+
+
+
+-- JOINS VVVVVVV
+
+
+-- INNER JOIN - Joining departments and dept_manager tables -- USING ALIAS
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+
+
+
+-- LEFT JOIN - Joining retirement_info and dept_emp tables -- NOT USING ALIAS
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	retirement_info.last_name,
+    dept_employees.to_date
+FROM retirement_info
+LEFT JOIN dept_employees
+ON retirement_info.emp_no = dept_employees.emp_no;
+
+-- LEFT JOIN - Joining retirement_info and dept_emp tables -- USING ALIASES
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+-- Create table to hold info
+INTO current_emp
+
+FROM retirement_info as ri
+LEFT JOIN dept_employees as de
+ON ri.emp_no = de.emp_no
+-- Add a filter with the date 9999-01-01
+WHERE de.to_date = ('9999-01-01');
+
+-- When this block of code is executed, a new table
+-- containing only the current employees who are eligible for retirement will be returned.
+SELECT * FROM current_emp;
+
+
+
+
+
+
+-- USE ALIASES
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	retirement_info.last_name,
+    dept_employees.to_date
+FROM retirement_info
+LEFT JOIN dept_employees
+ON retirement_info.emp_no = dept_employees.emp_no;
+
+-- Shorten to a nickname
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    de.to_date
+-- TELL SQL WHAT NAMES REFER TO
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+
+
+
+
 
