6bd5e568144f2ca5d8b7751d95b6489cef07e17f
lawnshogan <Shawnlogan.gis@gmail.com>
2022-05-02 17:33:03 -0600

    Starting 7.3.4

--- schema.sql
+++ schema.sql
@@ -187,11 +187,17 @@ SELECT ri.emp_no,
     de.to_date
 -- TELL SQL WHAT NAMES REFER TO
 FROM retirement_info as ri
-LEFT JOIN dept_emp as de
+LEFT JOIN dept_employees as de
 ON ri.emp_no = de.emp_no;
 
 
 
-
-
+-- Count, Group By, and Order By
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_employees as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
 
