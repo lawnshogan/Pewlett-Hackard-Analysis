d6528d80f2d21f5eb9bbd772f5b674e44eb6a0e2
lawnshogan <Shawnlogan.gis@gmail.com>
2022-05-03 15:12:14 -0600

    Middle 7.3.5

--- schema.sql
+++ schema.sql
@@ -202,10 +202,46 @@ ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
 
-
---SELECT * INTO num_employees_retiring FROM 'C:\Users\shawn\DataScienceMaster\Modules\Module_7_SQL\Repository\Pewlett-Hackard-Analysis\Data';
-
-
 SELECT * FROM num_employees_retiring;
 
 DROP TABLE num_employees_retiring;
+
+
+-- Employee Information - 1)Select columns from three tables, 2)Create a new temp table, 
+-- 3)Add aliases, 4)Join two of the three tables
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no, first_name, last_name, gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+DROP TABLE emp_info;
+
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+-- Third Join
+INNER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
+	
+SELECT * FROM emp_info;
+
+
+
+
+
+
