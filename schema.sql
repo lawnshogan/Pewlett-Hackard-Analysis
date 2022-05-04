cfd7279a6f622f843fdc6bdc7f63e056688b9cb1
lawnshogan <Shawnlogan.gis@gmail.com>
2022-05-03 18:22:12 -0600

    Middle 7.3.5

--- schema.sql
+++ schema.sql
@@ -187,7 +187,7 @@ DROP TABLE retirement_info_dept;
 SELECT * FROM retirement_info_dept;
 
 -------------------------------------------------------------------------------------
--- Employee List
+-- LIST 1: Employee salary List
 -- Salaries - Employee Number, FName, LName, Gender, to_date, Salary
 SELECT * FROM salaries
 ORDER BY to_date DESC;
@@ -203,48 +203,64 @@ INTO emp_info
 FROM employees as e
 INNER JOIN salaries as s
 ON (e.emp_no = s.emp_no)
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 
-	
-	
-	
-	
-	
-	
-	
-
-
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
--- Third Join
 INNER JOIN dept_employees as de
 ON (e.emp_no = de.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
-     AND (de.to_date = '9999-01-01');
-
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+	
 DROP TABLE emp_info;
-SELECT * FROM emp_info
+SELECT * FROM emp_info;
+
+
+-- LIST 2: Management List
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+
+	
+DROP TABLE manager_info;
+SELECT * FROM manager_info;
+
+
+-- List 3: Department Retirees
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_employees AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
 
 
 
+DROP TABLE dept_info;
+SELECT * FROM dept_info;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+------------------------------------------------------
+-- Query employee numbers, employee first name, employee last name, employee department name
+-- Find all employees in the developement and sales depatment
+SELECT emp_no, first_name, last_name, dept_name
+FROM dept_info
+WHERE dept_name IN('Sales', 'Development');
 
 
 
