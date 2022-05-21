282424062ea15e2bf8af4fd3e7afd6da4cf1ca02
lawnshogan <Shawnlogan.gis@gmail.com>
2022-05-21 12:03:59 -0600

    h

--- schema.sql
+++ schema.sql
@@ -307,29 +307,29 @@ ORDER BY COUNT(title) DESC;
 
 SELECT * FROM retiring_titles;
 
---Deliverable 2
+-- Deliverable 2
 --Mentorship Eligibility table
 
-SELECT DISTINCT ON(e.emp_no)e.emp_no,
-	e.first_name,
-	e.last_name,
-	e.birth_date,
-	de.from_date,
-	de.to_date,
-	t.title
---DISTINCT ON(emp_no)
-INTO mentorship_table
-FROM employees AS e
-	INNER JOIN dept_emp AS de
-		ON (e.emp_no = de.emp_no)
-			INNER JOIN titles AS t
-				ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+SELECT DISTINCT ON (e.emp_no) 
+    	e.emp_no,
+	   e.first_name,
+    	e.last_name,
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
-ORDER BY emp_no;
+ORDER BY e.emp_no, ti.from_date DESC;
 
-
-
+DROP TABLE mentorship_eligibility;
+SELECT * FROM mentorship_eligibility;
 
 
 
